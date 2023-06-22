a8b5460a98dd80cd06d841d3a9aaf579a530352f
Marcos <msalrubio@gmail.com>
2023-06-22 19:15:39 +0200

    feat: Added Simple Blog App

---
+++ src/components/PostItem.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+  import { Post } from '@/models/Post';
+  const props = defineProps<{
+    post: Post;
+  }>();
+</script>
+
+<template>
+  <article class="card">
+    <article>
+      <a href="" :aria-label="props.post.title">
+        <img class="card-img" src="https://via.placeholder.com/150/24f355" />
+        <div class="card-info">
+          <h3>{{ props.post.title }}</h3>
+          <p class="card-lead">
+            {{ props.post.body }}
+          </p>
+        </div>
+      </a>
+    </article>
+  </article>
+</template>
+
+<style scoped>
+  .card {
+    min-height: 10rem;
+    background: #fff;
+    box-shadow: 0 0.2rem 1rem rgba(0, 0, 0, 0.05);
+    cursor: pointer;
+    transition: transform 0.3s, box-shadow 0.3s;
+  }
+  .card a {
+    color: #333;
+    text-decoration: none;
+  }
+  .card:hover,
+  .card:focus {
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+    transform: translateY(-0.5rem);
+  }
+  .card:focus {
+    outline: 5px solid var(--primary-color);
+  }
+  .card-img {
+    width: 100%;
+  }
+  .card-info {
+    padding: 0 1rem 1rem;
+  }
+</style>
---
+++ src/components/PostsList.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+  import { onMounted, reactive } from 'vue';
+  import { Post } from '@/models/Post';
+  import PostItem from '@/components/PostItem.vue';
+
+  let posts = reactive<Post[]>([]);
+  onMounted(() => {
+    setTimeout(() => {
+      loadPosts();
+    }, 2000);
+  });
+
+  const loadPosts = () => {
+    console.log('Cargamos los posts');
+    posts = [
+      ...posts,
+      {
+        userId: 1,
+        id: 1,
+        title:
+          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+        body: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'qui est esse',
+        body: 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla',
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+        body: 'et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut',
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: 'eum et est occaecati',
+        body: 'ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit',
+      },
+    ];
+  };
+</script>
+
+<template>
+  <section class="dashboard">
+    <div class="dashboard-posts">
+      <PostItem v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  .dashboard {
+    max-width: 90rem;
+    margin: 0 auto;
+    padding: 2rem;
+  }
+
+  .dashboard-posts {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr));
+    gap: 1.5rem;
+    margin-bottom: 2rem;
+  }
+</style>
